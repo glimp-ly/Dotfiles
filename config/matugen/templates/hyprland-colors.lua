e5034c39e9bfdd0cb6870168a654990aec434f74
glimp-ly <glimp.dev@gmail.com>
2026-08-25 21:03:11 -0500

    punto control

---
+++ config/matugen/templates/hyprland-colors.lua
@@ -0,0 +1,8 @@
+-- Generado automáticamente por Matugen v3
+return {
+<* for name, value in colors *>
+    ["{{ name }}"] = "rgb({{ value.default.hex_stripped }})",
+<* endfor *>
+    active_border = "rgba({{ colors.primary.default.hex_stripped }}ee)",
+    inactive_border = "rgba({{ colors.surface_container.default.hex_stripped }}aa)",
+}
